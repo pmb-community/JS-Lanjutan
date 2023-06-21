f81271ebf05d56feeb9a6e58997b9762ba33d3d5
unknown <richardowen2411@gmail.com>
2023-06-21 10:26:18 +0800

    first commit

---
+++ 4.js
@@ -0,0 +1,23 @@
+// Tipe data dan map()
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+const bioData = {
+  nama: "kucing",
+  umur: 12
+};
+
+const bioDataMahasiswa = [
+  {
+    nama: "ersan",
+    umur: 40
+  },
+  {
+    nama: "dika",
+    umur: 23
+  },
+  {
+    nama: "ales",
+    umur: 40,
+  },
+];
---
+++ 5/script.js
@@ -0,0 +1,52 @@
+const cacheKey = "DATAMAHASISWA";
+let dataCache = [];
+
+const inputNama = document.querySelector("#nama");
+const inputUmur = document.querySelector("#umur");
+const inputProdi = document.querySelector("#prodi");
+
+const submit = document.querySelector("#submit");
+
+const table = document.querySelector("#table");
+
+submit.addEventListener("click", function () {
+  const data = {
+    nama: inputNama.value,
+    umur: inputUmur.value,
+    prodi: inputProdi.value
+  };
+
+  const element = `
+    <tr>
+      <td>${data.nama}</td>
+      <td>${data.umur}</td>
+      <td>${data.prodi}</td>
+    </tr>
+  `
+
+  table.innerHTML += element;
+
+  dataCache.push(data);
+  localStorage.setItem(cacheKey, JSON.stringify(dataCache));
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem(cacheKey) === 'undefined') {
+    localStorage.setItem(cacheKey, JSON.stringify(dataCache));
+  } else {
+    dataCache = JSON.parse(localStorage.getItem(cacheKey));
+    console.log(dataCache)
+
+    dataCache.map(function (data) {
+      const element = `
+        <tr>
+          <td>${data.nama}</td>
+          <td>${data.umur}</td>
+          <td>${data.prodi}</td>
+        </tr>
+      `;
+
+      table.innerHTML += element;
+    })
+  }
+})
